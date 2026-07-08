3c6b9b3bf2e8243741744b48caaee4638fd47a37
embeddedude <149589098+embeddedude@users.noreply.github.com>
2026-07-08 14:16:31 +0530

    arrays.sv

--- arrays.sv
+++ arrays.sv
@@ -1,8 +1,8 @@
 //packed arrasy of type(1d,2d,3d)
 module tb;
-  int [7:0] arr;
+  bit [7:0] arr;
   initial begin
-  arr = 
+  arr = 8'b10101010
   $display("the value is %b",arr);
   end
 endmodule
