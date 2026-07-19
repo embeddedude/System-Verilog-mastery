def3b00ffcbf056cf628876d5f23b5fe27dbbb6b
embeddedude <149589098+embeddedude@users.noreply.github.com>
2026-07-19 23:02:52 +0530

    classes.sv

--- classes.sv
+++ classes.sv
@@ -1,1 +1,17 @@
+class myclass;
+  int x;
+  int y;
+  
+  function byte mul(int x,int y);
+    return x*y;
+  endfunction
+endclass
+
+module tb;
+    myclass obj1;
+    initial begin
+    obj1 = new();
+      $display("result = %0d",obj1.mul(10,5));
+    end
+endmodule
 
