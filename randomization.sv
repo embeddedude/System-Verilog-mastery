e0bcc64c1b0d106dfde8e9630f741370d2b8b2a1
embeddedude <149589098+embeddedude@users.noreply.github.com>
2026-07-28 23:38:12 +0530

    randomization.sv

--- randomization.sv
+++ randomization.sv
@@ -1,1 +1,20 @@
 
+class packet;
+  rand int data;
+  constraint data_range{ 
+     data inside{[1000:4000]};
+  }
+endclass
+                      
+module tb;
+  packet p = new();
+  initial begin
+    repeat(8) begin
+      p.randomize();
+      $display("%0d",p.data);
+    end
+  end
+endmodule
+
+      
+    
