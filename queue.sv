7b34aafafb9f2e17ce9ca6b29eb702c5d884af14
embeddedude <149589098+embeddedude@users.noreply.github.com>
2026-07-31 00:54:45 +0530

    queue.sv

--- queue.sv
+++ queue.sv
@@ -1,1 +1,21 @@
-
+// Code your testbench here
+// or browse Examples
+// push pop display 
+class q;
+  rand int queue[$];
+  constraint q_size{queue.size() == 5; }
+  constraint q_data{
+    foreach(queue[i])
+      queue[i] inside {[10:20]};}
+endclass
+module tb;
+  initial begin
+    q p1 = new();
+    p1.randomize();
+    p1.queue.push_front(5);
+    $display("%p",p1);
+    p1.queue.pop_back();
+    p1.queue.pop_front();
+    $display("%p",p1);
+  end
+endmodule
