c588e232d321c353ec21e6f585319f8f34e297c3
embeddedude <149589098+embeddedude@users.noreply.github.com>
2026-08-04 23:55:38 +0530

    randomization.sv

--- randomization.sv
+++ randomization.sv
@@ -17,4 +17,23 @@ module tb;
 endmodule
 
       
-    
+    // Code your testbench here
+// or browse Examples
+class packet;
+  randc int data;
+  constraint c_data {data inside{[30:50]};}
+  function int display();
+    $display("the value of the packet is %d",data);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    packet p1 = new();
+    repeat(15) begin
+      p1.randomize();
+      p1.display();
+      
+    end
+  end
+endmodule
